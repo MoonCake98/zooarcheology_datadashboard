370a8d9fb59d6a8e637dbdb41f85a9f8353fd659
Mooncake98_laptop <t.t.vo@st.hanze.nl>
2024-12-04 10:06:07 +0100

    initial panel application test and reading , filtering and displaying of the data

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
-import pandas as pd
+import pandas as pd # import pandas for data handling purposes
 
 import panel as pn # import panel for ui purposes
+
 
 # load in all the java extensions etc for the panel server
 pn.extension()
 
+# define data file path
 dataframe_filepath = "~/Downloads/f07bce4f-b08c-fe92-6505-c9e534d89a09--v1--full.csv"
 
-pn.pane.Alert("render of representation of the dataframe").servable()
-pn.pane.Alert("current pandas version " + pd.__version__).servable()
-
-
-
-def read_dataframe(dataframe_filepath):
-    df = pd.read_csv(dataframe_filepath, low_memory=False)
+# define data reading function
+def read_dataframe(df_filepath):
+    """reading in of the datafile into a pandas dataframe"""
+    df = pd.read_csv(df_filepath, low_memory=False)
     return df
 
 # read dataframe
 df = read_dataframe(dataframe_filepath)
 
-# display dataframe
-pn.pane.DataFrame(df.head()).servable()
+#select collumns with variables of interest of the dataframe to avoid visual clutter
+display_df = df[df.columns[[1,2,4,6,7,8,26,27]]]
 
-#special indicators with dif numbers based on amount
-pn.indicators.Number(
-    name="Wind Speed",
-    value=8.6,
-    format="{value} beh",
-    colors=[(10, "green"), (100, "red")],
-).servable()
+
+# display head of dataframe to get a visualisation of the dataframe
+df_pane = pn.pane.DataFrame(display_df.head())
+
