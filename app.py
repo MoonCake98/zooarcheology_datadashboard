b851ff2054824b9ecf185ac33a317b75862fcd7b
Mooncake98_laptop <t.t.vo@st.hanze.nl>
2024-12-06 16:18:00 +0100

    added the unnique values per column plot

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import panel as pn # import panel for ui purposes
 
 import folium as fl
 
+import matplotlib.pyplot as plt
+
 
 # load in all the java extensions etc for the panel server
 pn.extension()
@@ -40,6 +42,7 @@ mean_panel = pn.pane.DataFrame(mean_df)
 # put 3 dataframes into a single row
 df_row = pn.Row(pn.Column(df_display_head_panel,mean_panel),df_unique_coordinates_panel)
 
+## create a map to represent the 15 different coordinates found in the dataset
 # map centered on the mean of all the unique coordinates in the data
 coordinate_map = fl.Map(location=[mean_df["Latitude (WGS-84)"], mean_df["Longitude (WGS-84)"]], zoom_start=12)
 interactive_map_panel = pn.pane.plot.Folium(coordinate_map, height=400)
@@ -54,16 +57,43 @@ for latitude, longititude,project in zip(unique_coordinates_df["Latitude (WGS-84
 interactive_map_panel.object = coordinate_map
 
 
+## generate plot to showcase unique value distribution amongst columns of the dataframe
+# filter the dataframe for uniques
+df_uniques = df.nunique()
 
+# generate plot figure
+plt.figure(figsize=(15, 8))
+df_uniques.plot(kind='bar', color='skyblue', edgecolor='black')
+
+# plot labels
+plt.xlabel("Columns")
+plt.ylabel("Number of Unique Values")
+plt.title("Unique Values per Column")
+
+# plot column label distances and orientation
+plt.xticks(ticks=range(len(df_uniques.index)),ha="right",va="top", labels=df_uniques.index,rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# add a line ot show the total amount of rows
+plt.axhline(y=239320, color='red', linestyle='--', linewidth=2, label='total rows')
+
+# log plot cause very broad value range
+plt.yscale('log')
+
+# add legend
+plt.legend()
+
+# avoid overlap
+plt.tight_layout()
+
+plot_pane = pn.pane.Matplotlib(plt.gcf(), dpi=100)
 
 # put the contents of the above elemnts into a column to get a single object for the whole first page
-fullpage1_collumn = pn.Column(alert_panel_text_page1,alert_panel_pandasversion,df_row)
-
-
+fullpage1_collumn = pn.Column(alert_panel_text_page1,alert_panel_pandasversion,df_row,plot_pane)
 
 # make placeholder alert and markdown title panel for future geographical visualisation
-markdown_panel_title_page2 = pn.pane.Markdown("# future geographical visualisation")
-alert_panel_text_page2 = pn.pane.Alert("this is simply a placeholder, the actual figure has yet to be finished")
+markdown_panel_title_page2 = pn.pane.Markdown("#geographical visualisation")
+alert_panel_text_page2 = pn.pane.Alert("ask about marker clustering plugin (https://python-visualization.github.io/folium/latest/user_guide/plugins/marker_cluster.html)")
 
 # add placeholders together into a column to get a single object for the second page
 fullpage2_collumn = pn.Column(markdown_panel_title_page2, alert_panel_text_page2,interactive_map_panel)
