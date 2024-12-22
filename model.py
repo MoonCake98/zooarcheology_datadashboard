080406aa557951b984a44895f6adb06e05706ef0
Mooncake98_laptop <t.t.vo@st.hanze.nl>
2024-12-22 16:53:00 +0100

    added get_subset_df function and implemented it in the get_column_unique_values funciton

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import hvplot.pandas
 
 class Model_example:
     def __init__(self, filepath):
@@ -7,28 +7,33 @@ class Model_example:
 
         self.filepath = filepath # define the filepath as a property of the object
 
-        self.df = self.read_dataframe() # read in the data corresponding to the previously provided filepath
+        self.df = self.read_dataframe() # read in the data corresponding to the previously provided filepath\
+        
+        self.idf = self.df.interactive()
 
         self.unique_coordinates_df = self.get_unique_coordinates()
 
         self.mean_coordinates = self.unique_coordinates_df.mean() # get the mean of the unique coords to get a map starting point
 
-    def get_unique_coordinates(self):
+
+    def get_unique_coordinates(self) -> pd.DataFrame:
         """get unique coords from dataset columns 7 and 8"""
         return self.df[self.df.columns[[7, 8]]].drop_duplicates()
 
 
-    def read_dataframe(self):
+    def read_dataframe(self) -> pd.DataFrame:
         """read dsv file into pandas df"""
         # note that the pandas read fucniton has to be called with the low_memory variable as False
         # this is because of the size of the dataset
         return pd.read_csv(self.filepath, low_memory=False)
-    
-    def get_column_unique_values(self, column_name):
-        """get unique values from a specific column"""
-        return self.df[column_name].unique()
 
-    def count_na_and_actual_values(self):
+
+    def get_column_unique_values(self, column_name) -> pd.Series :
+        """get unique values from a specific column"""
+        return self.get_subset_df(column_name).unique()
+
+
+    def count_na_and_actual_values(self) -> pd.DataFrame:
         """calculate counts of N/A-like and actual values for each column"""
         na_counts = {}
         actual_counts = {}
@@ -40,3 +45,7 @@ class Model_example:
             actual_counts[column] = actual_count
 
         return na_counts, actual_counts
+    
+    def get_subset_df(self,columns) -> pd.DataFrame:
+        """get a subset of the dataframe with only the columns provided as arguments"""
+        return self.df[columns]
